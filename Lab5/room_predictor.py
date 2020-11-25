111e8a6c0fa63fc3f83f10055510ad80bed824b6
Isak Nyberg <36712644+IsakNyberg@users.noreply.github.com>
2020-11-25 18:14:33 +0100

    added the submission files for lab 5

--- Lab5/room_predictor.py
+++ Lab5/room_predictor.py
@@ -1,10 +1,10 @@
+# Author: Isak Nyberg, Rahul Sharma Kothuri
 from sklearn import tree
 import csv
 
-with open('data.csv', newline='') as f:
+with open('data.csv', newline='') as f:   # import data from dataset downloaded from the Internet. 
     reader = csv.reader(f)
     raw = list(reader)
-
 
 data = []
 labels = []
@@ -18,8 +18,8 @@ for raw_datapoint in raw[1:]: # every data point in the raw data ignoring the ta
         raw_datapoint[9], # condition
         raw_datapoint[13], # year built
     ]
-    datapoint = [round(float(entry)) for entry in datapoint] # make all entries integers
-    labels.append(round(float(raw_datapoint[3]))) # number of bathrooms (independent variable)
+    datapoint = [round(float(entry)) for entry in datapoint]    # make all entries integers
+    labels.append(round(float(raw_datapoint[3])))               # number of bathrooms (independent variable)
 
     data.append(datapoint)
 
@@ -29,18 +29,12 @@ preidction_labels = labels[:len(labels)//10]    # use 10% of data for prediction
 training_data = data[len(data)//10:]            # use the remaining 90% for training
 training_labels = labels[len(labels)//10:]      # use the remaining 90% for training
 
-clf = tree.DecisionTreeClassifier()
-clf.fit(training_data, training_labels)
+clf = tree.DecisionTreeClassifier() #create object
+clf.fit(training_data, training_labels) #train
 
-predictions = clf.predict(preidction_data)
+predictions = clf.predict(preidction_data)  # make prediction on 10% of the dataset to compare the accuracy
+correct_predictions = sum([preidction_labels[i] == predictions[i] for i in range(len(predictions))])  # count the number of correct predictions
 
-correct_predictions = 0
-for i in range(len(predictions)):
-    if preidction_labels[i] == predictions[i]:
-        correct_predictions += 1
-
-print(correct_predictions, len(predictions), 100*correct_predictions/len(predictions), '%')
-
-
-
-
+print("Correct predictions:",correct_predictions)
+print("Total predictions:", len(predictions))
+print("accuracy:", 100*correct_predictions/len(predictions), '%')
