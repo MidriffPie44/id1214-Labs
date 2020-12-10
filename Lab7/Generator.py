1f405e2b5a52dd48ced1d542543f34f40db488b7
Swindell17520 <59296149+Swindell17520@users.noreply.github.com>
2020-12-10 17:02:27 +0100

    Added the generator

---
+++ Lab7/Generator.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 10 13:51:10 2020
+
+@author: Rahul Kothuri, Isak Nyberg
+"""
+from nltk import Nonterminal,nonterminals,Production,CFG
+from nltk.parse.generate import generate
+
+grammar = CFG.fromstring("""
+        S ->  NP VP 
+        NP -> Det 
+        VP -> NLNP VBP Adj |LN PV RB |LN PV | LN VBZ NP | LNP V RB | NLN VBZ Adj | LN LVBZ 'the' LN
+        Det -> 'Those'
+        Det -> 'A'
+        Det -> 'the'
+        Det -> 'that'
+        Det -> 'The'
+        LN -> 'girl' | 'boy' | 'dog' 
+        LNP -> 'boys'
+        NLN -> 'house'
+        NLNP -> 'crackers'
+        V -> 'run' 
+        PV -> 'runs'| 'eats'
+        VBP -> 'are'
+        VBZ -> 'is'
+        LVBZ -> 'likes'
+        RB -> 'fast'
+        Adj ->'good'
+        EAdj -> 'happy'
+        Adj -> 'big'
+    """)
+for sentence in generate(grammar,depth=4):
+    print(' '.join(sentence))
